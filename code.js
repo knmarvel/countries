cb1e7d14a6cadb31488efd3c0efb68fad9b84fb2
Kano Marvel <knmarvel@gmail.com>
2021-07-29 16:24:05 -0400

    working on search

--- code.js
+++ code.js
@@ -20,8 +20,37 @@ const displayCountry = function (country){
     console.log(country)
 }
 
+const getAllCountries = function () {
 fetch(COUNTRY_URL + "all")
     .then(response => response.json())
     .then(data => data
         .forEach(displayCountry))
+    }
+
+// getAllCountries()
+
+// search
+const input = document.createElement("input")
+input.setAttribute(id="input", "Search Input")
+main.append(input)
+
+const button = document.createElement("button")
+button.setAttribute(id="button", "Search!")
+button.innerText = "Search!"
+main.append(button)
+
+const handleClick = function (event){
+    event.preventDefault()
+    console.log(input.value)
+
+    //search API for input value
+    let search_url = COUNTRY_URL + `/name/${input.value}`
+
     
+
+    //clear the search box
+    input.value = ""
+}
+
+button.addEventListener("click", handleClick)
+
